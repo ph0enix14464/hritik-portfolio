5ea753274e907b0e5ae64d2312846e0495bcf44e
Hritik Jung Basnet <hritik.jung@gmail.com>
2026-05-25 19:15:39 +0100

    Updates

--- _next/static/chunks/0h~-t54dt.hq6.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/169uzd~vsz2.1.js",
-  "static/chunks/0s_y_bxb._vu7.js",
-  "static/chunks/05fm4gxcqjl48.css",
-  "static/chunks/turbopack-16-62uvpdf9uu.js"
-])
--- _next/static/chunks/0itl52wodi13d.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/0eh2ih3179wp2.js",
-  "static/chunks/0s_y_bxb._vu7.js",
-  "static/chunks/turbopack-01s86sezop1p4.js"
-])
